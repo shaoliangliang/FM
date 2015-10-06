4eef92b2edea602669b09cc948ebdde5ab30d859
lanou3g <lanou3g@lanou3gdeMac-mini-89.local>
2015-10-06 14:45:17 +0800

    add code

---
+++ FM/FM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 邵亮亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FM/FM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FM
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 邵亮亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
